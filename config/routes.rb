e4c1fd2573a013d143275042483a88e02e443ff7
Daisuke Tabuchi <t-da@suou.waseda.jp>
2017-04-09 19:48:23 +0000

    201704100448

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'pictures' => 'pictures#index'
+
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
     end
   end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
